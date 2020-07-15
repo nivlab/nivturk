37b2d8b9c7fda3c6ef46f27a768902e8244ac3d9
Sam Zorowitz <szorowi1@gmail.com>
2020-07-15 14:12:07 -0400

    minor changes to PR - session clear within session initialization - add suspected incognito test case

--- app/__init__.py
+++ app/__init__.py
@@ -20,16 +20,16 @@ if not os.path.isdir(meta_dir): os.makedirs(meta_dir)
 reject_dir = os.path.join(ROOT_DIR, cfg['IO']['REJECT'])
 if not os.path.isdir(reject_dir): os.makedirs(reject_dir)
 
+## Check Flask mode; if debug mode, clear session variable.
+debug = cfg['FLASK']['DEBUG'] != "FALSE"
+if debug:
+    msg = "WARNING: Flask currently in debug mode. This should be changed prior to production."
+    warnings.warn(msg)
+
 ## Check Flask password.
 if cfg['FLASK']['SECRET_KEY'] == "PLEASE_CHANGE_THIS":
     msg = "WARNING: Flask password is currently default. This should be changed prior to production."
     warnings.warn(msg)
-
-## Check Flask mode; if debug mode, clear session variable.
-if cfg['FLASK']['DEBUG'] != "FALSE":
-    msg = "WARNING: Flask currently in debug mode. This should be changed prior to production."
-    warnings.warn(msg)
-    session.clear()
 
 ## Initialize Flask application.
 app = Flask(__name__)
@@ -46,11 +46,14 @@ app.register_blueprint(error.bp)
 @app.route('/')
 def index():
 
+    ## Debug mode: clear session.
+    if debug:
+        session.clear()
+
     ## Store directories in session object.
     session['data'] = data_dir
     session['metadata'] = meta_dir
     session['reject'] = reject_dir
-    session['debug'] = cfg['FLASK']['DEBUG'] != "FALSE"
 
     ## Record incoming metadata.
     info = dict(
@@ -75,7 +78,18 @@ def index():
         ## Redirect participant to error (admin error).
         return redirect(url_for('error.error', errornum=1001))
 
-    ## Case 3: repeat visit, manually changed workerId.
+    ## Case 3: repeat visit, preexisting log but no session data (suspected incognito).
+    elif not 'workerId' in session and info['workerId'] in os.listdir(meta_dir):
+
+        ## Update metadata.
+        session['workerId'] = info['workerId']
+        session['ERROR'] = '1004: suspected incognito user.'
+        write_metadata(session, ['ERROR'], 'a')
+
+        ## Redirect participant to error (unusual activity).
+        return redirect(url_for('error.error', errornum=1004))
+
+    ## Case 4: repeat visit, manually changed workerId.
     elif 'workerId' in session and session['workerId'] != info['workerId']:
 
         ## Update metadata.
@@ -85,8 +99,8 @@ def index():
         ## Redirect participant to error (unusual activity).
         return redirect(url_for('error.error', errornum=1002))
 
-    ## Case 4: repeat visit, preexisting activity.
-    elif 'workerId' in session or info['workerId'] in os.listdir(meta_dir):
+    ## Case 5: repeat visit, preexisting activity.
+    elif 'workerId' in session:
 
         ## Update metadata.
         session['WARNING'] = "Revisited home."
@@ -95,7 +109,7 @@ def index():
         ## Redirect participant to consent form.
         return redirect(url_for('consent.consent'))
 
-    ## Case 5: first visit, workerId present.
+    ## Case 6: first visit, workerId present.
     else:
 
         ## Update metadata.
