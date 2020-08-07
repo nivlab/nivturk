682f22aa98e08725c7bbc551fc0a67093ad3ff4a
Sam Zorowitz <szorowi1@gmail.com>
2020-08-06 23:41:49 -0400

    initial push

--- app/error.py
+++
@@ -1,9 +0,0 @@
-from flask import (Blueprint, redirect, render_template, request, session, url_for)
-
-## Initialize blueprint.
-bp = Blueprint('error', __name__)
-
-@bp.route('/error/<int:errornum>')
-def error(errornum):
-    """Present error message to participant"""
-    return render_template('error.html', errornum=errornum)
--- app/utils.py
+++
@@ -1,5 +0,0 @@
-import random, string
-
-def gen_code(N):
-    """Generate random completion code."""
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=N))
