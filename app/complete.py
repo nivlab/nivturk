7b1d3f0e4bdac5fb1540990eb2d7f8e7b39656a0
Sam Zorowitz <szorowi1@gmail.com>
2020-08-10 23:31:22 -0400

    version 1.0 (MTurk) (#42)
    * version 1.0
    
    * update error page

--- app/complete.py
+++ app/complete.py
@@ -8,21 +8,52 @@ bp = Blueprint('complete', __name__)
 def complete():
     """Present completion screen to participant."""
 
-    ## Error-catching: screen for previous visits.
-    if 'complete' in session:
+    ## Access query string.
+    query_info = request.args
 
-        ## Update participant metadata.
-        session['WARNING'] = "Revisited complete page."
+    ## Confirm all TurkPrime metadata present.
+    fields = ['workerId','assignmentId','hitId','a','tp_a','b','tp_b','c','tp_c']
+    all_fields = all([f in query_info for f in fields])
+
+    ## Error-catching: screen for missing session.
+    if not 'workerId' in session:
+
+        ## Redirect participant to error (missing workerId).
+        return redirect(url_for('error.error', errornum=1000))
+
+    ## Case 1: visit complete page with previous rejection.
+    elif session.get('complete') == 'reject':
+
+        ## Update metadata.
+        session['WARNING'] = "Revisited complete."
         write_metadata(session, ['WARNING'], 'a')
 
+        ## Redirect participant to error (unusual activity).
+        return redirect(url_for('error.error', errornum=1005))
+
+    ## Case 2: visit complete page with previous error.
+    elif session.get('complete') == 'error':
+
+        ## Update metadata.
+        session['WARNING'] = "Revisited complete."
+        write_metadata(session, ['WARNING'], 'a')
+
+        ## Redirect participant to error (unusual activity).
+        return redirect(url_for('error.error', errornum=1005))
+
+    ## Case 3: visit complete page but missing metadata.
+    elif session.get('complete') == 'success' and not all_fields:
+
+        ## Update metadata.
+        session['WARNING'] = "Revisited complete."
+        write_metadata(session, ['WARNING'], 'a')
+
+        ## Redirect participant with complete metadata.
+        url = "/complete?workerId=%s&assignmentId=%s&hitId=%s&a=%s&tp_a=%s&b=%s&tp_b=%s&c=%s&tp_c=%s" %(session['workerId'], session['assignmentId'], session['hitId'], session['a'], session['tp_a'], session['b'], session['tp_b'], session['c'], session['tp_c'])
+        return redirect(url)
+
+    ## Case 4: all else.
     else:
 
-        ## Update participant metadata.
-        session['complete'] = True
-        write_metadata(session, ['complete'], 'a')
-
-    ## DEV NOTE:
-    ## If you want a custom completion code, replace the return statement with:
-    ## > render_template('complete.html', value=session['complete'])
-
-    return render_template('complete.html')
+        ## Redirect participant with completion code.
+        return render_template('complete.html')
--- app/consent.py
+++ app/consent.py
@@ -8,14 +8,50 @@ bp = Blueprint('consent', __name__)
 def consent():
     """Present consent form to participant."""
 
-    ## Case 1: first visit.
-    if not 'consent' in session:
+    ## Error-catching: screen for missing session.
+    if not 'workerId' in session:
+
+        ## Redirect participant to error (missing workerId).
+        return redirect(url_for('error.error', errornum=1000))
+
+    ## Case 1: previously completed experiment.
+    elif 'complete' in session:
+
+        ## Update metadata.
+        session['WARNING'] = "Revisited consent page."
+        write_metadata(session, ['WARNING'], 'a')
+
+        ## Redirect participant to complete page.
+        return redirect(url_for('complete.complete'))
+
+    ## Case 2: first visit.
+    elif not 'consent' in session:
 
         ## Present consent form.
         return render_template('consent.html')
 
-    ## Case 2: repeat visit, previous consent.
-    elif session['consent']:
+    ## Case 3: repeat visit, previous bot-detection.
+    elif session['consent'] == 'BOT':
+
+        ## Update participant metadata.
+        session['WARNING'] = "Revisited consent form."
+        write_metadata(session, ['WARNING'], 'a')
+
+        ## Redirect participant to error (unusual activity).
+        return redirect(url_for('error.error', errornum=1005))
+
+    ## Case 4: repeat visit, previous non-consent.
+    elif session['consent'] == False:
+
+        ## Update participant metadata.
+        session['WARNING'] = "Revisited consent form."
+        write_metadata(session, ['WARNING'], 'a')
+
+        ## Redirect participant to error (decline consent).
+        return redirect(url_for('error.error', errornum=1002))
+
+    ## Case 5: repeat visit, previous consent.
+    else:
 
         ## Update participant metadata.
         session['WARNING'] = "Revisited consent form."
@@ -23,17 +59,6 @@ def consent():
 
         ## Redirect participant to alert page.
         return redirect(url_for('alert.alert'))
-
-    ## Case 3: repeat visit, previous non-consent.
-    else:
-
-        ## Update participant metadata.
-        session['WARNING'] = "Revisited consent form."
-        write_metadata(session, ['WARNING'], 'a')
-
-        ## Redirect participant to error (decline consent).
-        return redirect(url_for('error.error', errornum=1003))
-
 
 @bp.route('/consent', methods=['POST'])
 def consent_post():
@@ -48,10 +73,12 @@ def consent_post():
 
         ## Update participant metadata.
         session['consent'] = 'BOT'
-        write_metadata(session, ['consent'], 'a')
+        session['experiment'] = False    # Prevents incognito users
+        session['complete'] = 'error'
+        write_metadata(session, ['consent','experiment','complete'], 'a')
 
         ## Redirect participant to error (unusual activity).
-        return redirect(url_for('error.error', errornum=1002))
+        return redirect(url_for('error.error', errornum=1005))
 
     ## Check participant response.
     elif subj_consent:
@@ -70,4 +97,4 @@ def consent_post():
         write_metadata(session, ['consent'], 'a')
 
         ## Redirect participant to error (decline consent).
-        return redirect(url_for('error.error', errornum=1003))
+        return redirect(url_for('error.error', errornum=1002))
